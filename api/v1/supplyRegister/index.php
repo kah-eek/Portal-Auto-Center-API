fd4143b8e39bf8a17e37d7c45679a16769c53218
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2018-05-27 21:42:48 -0300

    typo supply register

--- api/v1/supplyRegister/index.php
+++ api/v1/supplyRegister/index.php
@@ -10,7 +10,7 @@
 
 	if($_SERVER['REQUEST_METHOD'] == 'POST')
 	{
-		$supplyRegister = new RegisterSupply(
+		$supplyRegister = new SupplyRegister(
 			null,
 			$_POST["idTipoCombustivel"],
 			$_POST["idVeiculoCliente"],
